a2bbbb31739e0aa7ba011248e9e328cb02b20a68
assanmboge <mbogeassan77@gmail.com>
2020-03-27 22:07:19 +0200

    Remove node_modules from app

--- server.js
+++ server.js
@@ -4,8 +4,4 @@ const port = process.env.PORT || 3002;
 require('dotenv').config()
 
 const server = http.createServer(app);
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "shop-frontend", "build", "index.html"))
-})
-
 server.listen(port, () => console.log("Application running on port " + port));
